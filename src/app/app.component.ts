9a4a2914fc3d99633283002e451e5213c465cc8d
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-06-29 16:38:44 +0200

    Add SVG icons and illustrations, enhance HTML structure, and implement styles for portfolio sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InnovationComponent } from './components/innovation/innovation.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SocialLinksComponent } from './components/social-links/social-links.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    InnovationComponent,
+    ExperienceComponent,
+    HeroComponent,
+    NavigationComponent,
+    SocialLinksComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Laczynski.Me';
---
+++ src/app/components/innovation/innovation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-innovation',
+  standalone: true,
+  imports: [],
+  templateUrl: './innovation.component.html',
+  styleUrl: './innovation.component.scss',
+})
+export class InnovationComponent {}
